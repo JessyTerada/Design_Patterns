6703898dd9684ab7d4f491db05cf3a1f4d7b3bea
Jessica Tereda (Financial Services) <JE385653@wipro.com>
2017-08-17 14:38:55 -0300

    filling and cakecover class

--- src/main/java/Exercise2/CakeCover.java
+++ src/main/java/Exercise2/CakeCover.java
@@ -1,10 +1,14 @@
 package Exercise2;
 
-public class CakeCover {
+public class CakeCover extends HowToDo{
 
-	Ingredient chocolateSprinkle = new Ingredient("Chocolate sprinkle", 0.5, "tea-cup");
-	Ingredient chocolatePowder = new Ingredient("Chocolate powder", 1, "tea-cup");
-	Ingredient butter = new Ingredient("Butter", 2, "soup-poon");
-	Ingredient condensedMilk = new Ingredient("Condensed milk", 2, "can");
+	static Ingredient chocolateSprinkle = new Ingredient("Chocolate sprinkle", 0.5, "tea-cup");
+	static Ingredient chocolatePowder = new Ingredient("Chocolate powder", 1, "tea-cup");
+	static Ingredient butter = new Ingredient("Butter", 2, "soup-poon");
+	static Ingredient condensedMilk = new Ingredient("Condensed milk", 2, "can");
 
+	public static void main(String[] args) {
+		mixBurble(chocolateSprinkle.getNomeIng(), chocolatePowder.getNomeIng(), butter.getNomeIng(), condensedMilk.getNomeIng());
+		putCakeCover();
+	}
 }
--- src/main/java/Exercise2/Filling.java
+++ src/main/java/Exercise2/Filling.java
@@ -1,8 +1,14 @@
 package Exercise2;
 
-public class Filling {
+public class Filling extends HowToDo{
 
-	Ingredient chocolatePowder = new Ingredient("Chocolate powder", 0.5, "tea-cup");
-	Ingredient butter = new Ingredient("Butter", 2, "soup-poon");
-	Ingredient condensedMilk = new Ingredient("Condensed milk", 2, "cans");
+	static Ingredient chocolatePowder = new Ingredient("Chocolate powder", 0.5, "tea-cup");
+	static Ingredient butter = new Ingredient("Butter", 2, "soup-poon");
+	static Ingredient condensedMilk = new Ingredient("Condensed milk", 2, "cans");
+	
+	public static void main(String[] args) {
+		mixBurble(chocolatePowder.getNomeIng(), butter.getNomeIng(), condensedMilk.getNomeIng());
+		putFilling();
+	}
 }
+
